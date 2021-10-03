127e23790d09ee50ff905c66b6b6899788d3ad74
Rodrick Coleman <rscole87@gmail.com>
2021-10-03 14:21:22 -0400

    add about component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,12 +1,14 @@
 import React from "react";
 import NavComponent from "./NavComponent";
 import Jumbotron from "./ JumbotronComponent"
+import About from "./AboutComponent";
 
 const Main = () => {
   return (
     <>
         <NavComponent />
         <Jumbotron />
+        <About />
     </>
 )
 };
