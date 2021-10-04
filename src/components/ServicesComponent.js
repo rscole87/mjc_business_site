e5aa92fc6871a29404b5ca1e7048b65f26309afb
Rodrick Coleman <rscole87@gmail.com>
2021-10-03 20:05:00 -0400

    add cards to services component

--- src/components/ServicesComponent.js
+++ src/components/ServicesComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 const Services = () => {
   return (
@@ -13,13 +14,44 @@ const Services = () => {
           </div>
 
           <div className="row">
-            <div className="col"></div>
-            <div className="col"></div>
-            <div className="col"></div>
-          </div>
+            <div className="col">
+              <Card className="service-card">
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    Card subtitle
+                  </CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <button className="btn btn-primary">Learn More</button>
+                </CardBody>
+              </Card>
+            </div>
 
-          <div className="row">
-            <button className="btn btn-primary">Learn More</button>
+            <div className="col">
+              <Card className="service-card">
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    Card subtitle
+                  </CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <button className="btn btn-primary">Learn More</button>
+                </CardBody>
+              </Card>
+            </div>
+
+            <div className="col">
+              <Card className="service-card">
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">
+                    Card subtitle
+                  </CardSubtitle>
+                  <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                  <button className="btn btn-primary">Learn More</button>
+                </CardBody>
+              </Card>
+            </div>
           </div>
         </div>
       </section>
